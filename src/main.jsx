e14d5f3cc251d732634684fd37c07748fe152fbe
NouriHicham <nourichamidhicham@fpllefia.com>
2025-02-21 17:31:57 +0100

    arreglar cosas

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import '../node_modules/bootstrap/dist/js/bootstrap.js'
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
